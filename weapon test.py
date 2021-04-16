daac552c412e3383b8b7d0fda26a5f84df76d934
Procrastik <Firebaugh91@gmail.com>
2021-04-16 11:39:57 -0500

    4/16/21 update - My brain hurts with this stuff
    Started a test of weapon damage falloff calcs.

--- weapon test.py
+++ weapon test.py
@@ -1,60 +1,44 @@
 import json
 import traceback
 
-local_file = "E:\Roguetech\RogueTech\RogueModuleTech\VanillaWeapons\Weapon_Autocannon_AC10_0-STOCK.json"
+local_file = "E:\RT Analyzation\RT-Analyzation\Weapon_Gauss_RAILGUN.json"
 
 with open(local_file) as f:
     data = json.load(f)
 
-#weapon damage module - pulls the highest damage from base + any available modes and sets value to weapon_damage variable.
+
+#weapon damage falloff module, this will need to be after ranges are set!
+weapon_dam_falloff_min = 0
+weapon_dam_falloff_short = 0
+weapon_dam_falloff_medium = 0
+weapon_dam_falloff_long = 0
+weapon_dam_falloff_max = 0
 try:
-    max_mode_dam = 0 #set mode index of mode highest dam weapon
-    max_dam_mode = 0 #set value of highest additional damage in modes
-    weapon_damage = 0 #Damage modes loop max damage value
-    for i in range(len(data['Modes'])): #for loop to iterate over the number of Modes found
-        print('Damage mode', i)
-        try:
-            if data['Modes'][i]['DamagePerShot'] > max_mode_dam:
-                max_mode_dam = data['Modes'][i]['DamagePerShot']
-                max_dam_mode = i
+    distance_variance = data['DistantVariance']
+    if distance_variance > 0:
+        if data['DistantVarianceReversed'] == False:
             try:
-                if data['Modes'][i]['ShotsWhenFired']:#check for ShotsWhenFired in mode
-                    weapon_damage = data['Damage'] + max_mode_dam * data['ProjectilesPerShot'] * (data['ShotsWhenFired'] + data['Modes'][max_dam_mode]['ShotsWhenFired'])
+                weapon_dam_falloff_start = data['DamageFalloffStartDistance']
             except KeyError:
-                traceback.print_exc()
-                weapon_damage = data['Damage'] + max_mode_dam * data['ProjectilesPerShot'] * data['ShotsWhenFired']
-        except KeyError: #if no DamagePerShot in mode found
-            traceback.print_exc()
-            print('skipped')
-except KeyError: #removed indexerror as this should not throw one. This will catch errors when a weapon has no modes.
-    print('No modes. Reverting to base values.')
-    traceback.print_exc()
-    weapon_damage = data['Damage'] * (data['ProjectilesPerShot'] * data['ShotsWhenFired']) #damage = damage per shot * projectilespershot
-
-try:
-    max_shots_mode = 0 #set mode index of mode with highest shot count
-    max_mode_shots = 0 #set value of mode with most additional shots
-    weapon_damage2 = 0 #Shots modes loop max damage value
-    for i in range(len(data['Modes'])):
-        print('Shots loop:', i)
-        try:#if no damage in modes found then check modes for additional shots and calculate damage against base value
-            if data['Modes'][i]['ShotsWhenFired'] > max_mode_shots:
-                max_mode_shots = data['Modes'][i]['ShotsWhenFired']
-                max_shots_mode = i
+                weapon_dam_falloff_start = weapon_range_medium
             try:
-                weapon_damage2 = data['Damage'] * data['ProjectilesPerShot'] * (data['ShotsWhenFired'] + data['Modes'][max_shots_mode]['ShotsWhenFired']) #damage = damage per shot + max damage mode extra damage * projectilespershot * (shotswhenfiredbase + shotswhenfired in modes + damage in modes)
-                print(max_mode_shots)
-            except:
-                print('can this be reached?')
-                traceback.print_exc()
-        except:
-            weapon_damage2 = data['Damage'] + max_mode_shots * data['ProjectilesPerShot'] * data['ShotsWhenFired']   
-except KeyError: #removed indexerror as this should not throw one. This will catch errors when a weapon has no modes.
-    print('No modes. Reverting to base values.')
-    weapon_damage2 = data['Damage'] * (data['ProjectilesPerShot'] * data['ShotsWhenFired']) #damage = damage per shot * projectilespershot
+                weapon_dam_falloff_end = data['DamageFalloffEndDistance']
+            except KeyError:
+                weapon_dam_falloff_end = weapon_range_max
+            if weapon_range_min >= weapon_dam_falloff_start:
+                print('this should not happen?')
+            elif weapon_range_short > weapon_dam_falloff_start:
+                distance_ratio = (weapon_range_short - weapon_dam_falloff_start) / (weapon_dam_falloff_end - weapon_dam_falloff_start)
+               weapon_dam_falloff_short = (1.0 - (distance_ratio * distance_ratio)) * 
 
-if weapon_damage2 > weapon_damage:#checks the max damage mode and the max shots mode loop max damage values against each other and sets the highest 
-    weapon_damage = weapon_damage2
+            #standard tree
+        elif data['DistantVarianceReversed'] == True:
+            #reversed tree
 
-print(weapon_damage, weapon_damage2)
-print('Tonnage ', data['Tonnage'] )
+
+        print('do something')
+
+except KeyError:
+    print('No dam falloff, defaulting to zero.')
+    
+
